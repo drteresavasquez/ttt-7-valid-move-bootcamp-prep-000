de7cf3e65a6a444d490a84fc1ee1aadc56505d0b
Teresa Terry <dr.terry2013@gmail.com>
2018-02-07 16:53:44 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,17 @@
 # code your #valid_move? method here
-
+def valid_move?
+  if position_taken?
+    
+  else
+    
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(arr, index)
+  if (arr[index] == "X" || arr[index] == "O")
+    true
+  else
+    false
+  end
+end
